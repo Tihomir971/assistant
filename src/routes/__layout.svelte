a25618fe50d558cc844774d62e28fc90141f611f
Tihomir971 <72653027+Tihomir971@users.noreply.github.com>
2022-01-28 23:15:37 +0100

    Carbon added

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import 'carbon-components-svelte/css/g90.css';
+	import { Content, Grid } from 'carbon-components-svelte';
+	import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+
+<Content style="padding: 0">
+	<Grid fullWidth>
+		<slot />
+	</Grid>
+</Content>
