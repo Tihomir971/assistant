87c79fc3ff3edc9d995056c7584ae0193f6438e6
Tihomir971 <tihomir.jev@outlook.com>
2022-03-16 12:13:38 +0100

    coment Theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,18 @@
 <script>
 	import 'carbon-components-svelte/css/all.css';
-	import Theme from '$lib/ui/Theme.svelte';
+	// import Theme from '$lib/ui/Theme.svelte';
 	import Header from '$lib/ui/Header.svelte';
 	import { Content, Grid } from 'carbon-components-svelte';
 </script>
 
-<Theme persist>
-	<Header />
+<!-- <Theme persist> -->
+<Header />
 
-	<Content
-		style="background-image: url('../static/sun-tornado.svg'); background-size: cover;padding-left: 0; padding-right: 0; min-height: calc(100vh - 3rem)"
-	>
-		<Grid fullWidth>
-			<slot />
-		</Grid>
-	</Content>
-</Theme>
+<Content
+	style="background-image: url('../static/sun-tornado.svg'); background-size: cover;padding-left: 0; padding-right: 0; min-height: calc(100vh - 3rem)"
+>
+	<Grid fullWidth>
+		<slot />
+	</Grid>
+</Content>
+<!-- </Theme> -->
